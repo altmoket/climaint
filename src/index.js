08df783250b5d084cdb60df507f7bad15621dc9c
lele <leandro.nunez1457@gmail.com>
2024-12-20 01:41:06 -0500

    Add fountsource/roboto for material-ui

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom'; // For React 17, use 'react-dom'
 import App from './App'; // Import the main App component
 import './index.css'; // Import global styles
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // Render the App component into the root element
 ReactDOM.render(
