5e8af4df65dd6b6688b797573bc239fea495912c
lele <leandro.nunez1457@gmail.com>
2024-12-25 13:42:38 -0500

    Update Layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Box, Divider, Grid, Stack} from '@mui/material';
+import { Grid } from '@mui/material';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
 import Leftbar from '../components/Leftbar';
 
@@ -8,27 +8,14 @@ const Layout = () => {
   return (
     <>
       <ResponsiveAppBar />
-      {/* <Divider></Divider> */}
-      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        {/* <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-          <Grid item md={4} lg={3} display={{ xs: 'none', md: 'block' }} flex={true}>
-            <Leftbar />
-          </Grid>
-          <Grid item bgcolor={"red"} display="flex" justifyContent="center" height="100%">
-            <Box sx={{ textAlign: 'center' }}> 
-              <Outlet />
-            </Box>
-          </Grid>
-        </Grid> */}
-        <Stack direction={"row"}>
-          <Box width={"20%"}>
-            <Leftbar />
-          </Box>
-          <Box width={"stretch"}>
-            <Outlet />
-          </Box>
-        </Stack>
-      </Box>
+      <Grid container display={'flex'}>
+        <Grid item md={3} display={{ xs: 'none', sm: 'none', md: 'flex' }}>
+          <Leftbar />
+        </Grid>
+        <Grid item xs={12} sm={12} md={9}>
+          <Outlet />
+        </Grid>
+      </Grid>
     </>
   );
 };
