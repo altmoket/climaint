ff14e0be088bae417f98a7d116fb4c9d0ba68901
lele <leandro.nunez1457@gmail.com>
2024-12-25 12:20:10 -0500

    Update error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Container, Typography, Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Error() {
+  const navigate = useNavigate()
+
+  const handleGoHome = () => {
+    navigate('/')
+  };
+
   return (
-    <>
-      <p>Error</p>
-    </>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <Typography variant="h4" gutterBottom color="error">
+          Something went wrong!
+        </Typography>
+        <Typography variant="body1" paragraph>
+          We encountered an error while processing your request. Please try again later.
+        </Typography>
+        <Button variant="contained" color="primary" onClick={handleGoHome}>
+          Go to Home
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
