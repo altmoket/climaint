94097b3f51ae5b817064f3d2586ae43e0ee673c2
lele <leandro.nunez1457@gmail.com>
2024-12-27 01:10:29 -0500

    Move axiosInstance to utils

--- src/utils/clientUtils.js
+++ src/utils/clientUtils.js
@@ -1,4 +1,4 @@
-import axiosInstance from "../axiosInstance";
+import axiosInstance from "./axiosInstance";
 
 // Fetch interests with proper error handling
 export const getInterests = async (token) => {
